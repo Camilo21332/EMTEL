56fc28fe4f788fd510f0efc852e94db9bf1ce967
laravelBasic <pipicanodayana2005@gmail.com>
2025-02-11 23:14:23 -0500

    ajustes

--- Manejo_referencias_de_equipos/settings.py
+++ Manejo_referencias_de_equipos/settings.py
@@ -82,14 +82,22 @@ WSGI_APPLICATION = 'Manejo_referencias_de_equipos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'analisis',
+#        'USER': 'root',
+#        'PASSWORD': '12345678',
+#        'HOST':'localhost',
+#        'PORT': '3306',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'analisis',
-        'USER': 'root',
-        'PASSWORD': '12345678',
-        'HOST':'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+   
     }
 }
 
